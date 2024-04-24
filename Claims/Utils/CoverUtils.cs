85c399f07a0170741b129454425d2278f206cfa4
skeams <skeams@hotmail.com>
2024-04-24 21:40:56 +0200

    Task5: Premium-logic documented

--- Claims/Utils/CoverUtils.cs
+++ Claims/Utils/CoverUtils.cs
@@ -4,6 +4,19 @@ namespace Claims.Utils
 {
     public static class CoverUtils
     {
+        /**
+         * Computes Premium based on duration and type
+         *
+         * BaseRate of 1250 per day with an additional percentage on top based on type:
+         *  - Yacht: +10%
+         *  - PassengerShip: +20%
+         *  - Tanker: +50%
+         *  - Other: +30%
+         *
+         * Premium will accumulate as described above for the first 30 days.
+         * Then for the next 150, a 2% discount is applied. (or 5% in case of Yacht)
+         * After day 180 the discount is increased to 3% (8% for Yatch)
+         */
         public static decimal ComputePremium(DateOnly startDate, DateOnly endDate, CoverType coverType)
         {
             var premiumMultiplier = coverType switch
